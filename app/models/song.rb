c50bfdf5366888f1c7b9ab9985d780dcf55527c7
Alethea Cary <aletheacary@gmail.com>
2019-06-26 18:13:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    get
+    self.genre.name
   end
 
   def drake_made_this
-    Artist.find_by(name: "Drake")||= Artist.new(name: "Drake")
-    Artist.find_by(name: "Drake").songs << self
+    # Artist.find_by(name: "Drake")||= Artist.new(name: "Drake")
+    # Artist.find_by(name: "Drake").songs << self
   end
 end
